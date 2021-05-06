c0fdf78e1999d02da2a0d976647891a0fbe7c755
Waranyu-Mark26 <32408190+Waranyu-Mark26@users.noreply.github.com>
2021-05-07 00:46:05 +0700

    added navbar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
-import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
+import NavBar from './nav-bar'
 
 type Props = {
   preview?: boolean
@@ -11,8 +11,8 @@ const Layout = ({ preview, children }: Props) => {
   return (
     <>
       <Meta />
+      <NavBar />
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
---
+++ components/nav-bar.tsx
@@ -0,0 +1,68 @@
+import Container from "./container";
+import React, { FunctionComponent, useState } from "react";
+import { NAV_PATH } from "../lib/constants";
+import { MenuOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+
+type IMenuList = {
+  pathName: string;
+};
+
+const MenuList: FunctionComponent<IMenuList> = ({ pathName }: IMenuList) => {
+  return (
+    <li className="nav-item flex items-center">
+      <a
+        className="px-3 py-2 flex items-center text-xl uppercase leading-snug text-gray-600 hover:opacity-75"
+        href="#pablo"
+      >
+        {pathName}
+      </a>
+    </li>
+  );
+};
+
+const NavBar: FunctionComponent = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <nav className="relative flex flex-wrap items-center justify-between py-3 lg:py-10">
+      <Container>
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-700"
+              href="#pablo"
+            >
+              Edge Index
+            </a>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <MenuOutlined style={{ color: "#1d1d1d" }} />
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              {NAV_PATH.map((name) => (
+                <MenuList key={name} pathName={name} />
+              ))}
+            </ul>
+            <Button type="primary" shape="round" size="large">
+              Connect Wallet
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </nav>
+  );
+};
+
+export default NavBar;
