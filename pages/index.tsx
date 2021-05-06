9073408ce6c0ac6d0500d6dd27540097093799a6
Waranyu-Mark26 <32408190+Waranyu-Mark26@users.noreply.github.com>
2021-05-07 02:07:12 +0700

    added carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
+import MoreStories from "../components/section";
 import Layout from "../components/layout";
+import Intro from '../components/intro'
 import { getAllPosts } from "../lib/api";
 import Head from "next/head";
 import { CMS_NAME } from "../lib/constants";
@@ -13,8 +13,7 @@ type Props = {
 };
 
 const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+  const morePosts = allPosts.slice(0);
   return (
     <>
       <Layout>
@@ -22,16 +21,7 @@ const Index = ({ allPosts }: Props) => {
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
         <Container>
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
+          <Intro />
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
