1f81490a5282dba569ff30a07b3b9d8fc17a38de
poomsc <poom.genius@gmail.com>
2021-05-07 19:19:00 +0700

    edit template

--- pages/indices.tsx
+++ pages/indices.tsx
@@ -1,24 +1,21 @@
 import Container from "../components/container";
 import Layout from "../components/layout";
-import Intro from '../components/intro'
+import Intro from "../components/intro";
 import Head from "next/head";
 import { CMS_NAME } from "../lib/constants";
 import "antd/dist/antd.css";
-import { RecoilRoot } from 'recoil'
-
+import { RecoilRoot } from "recoil";
 
 const Stake = () => {
   return (
-    <RecoilRoot>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-        </Container>
-      </Layout>
-    </RecoilRoot>
+    <Layout>
+      <Head>
+        <title>Next.js Blog Example with {CMS_NAME}</title>
+      </Head>
+      <Container>
+        <Intro />
+      </Container>
+    </Layout>
   );
 };
 
