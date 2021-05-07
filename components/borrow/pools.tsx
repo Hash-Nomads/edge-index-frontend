68f1979cdd35c323d94f913b7a5259e91a8334b1
Waranyu-Mark26 <32408190+Waranyu-Mark26@users.noreply.github.com>
2021-05-08 02:12:07 +0700

    Merge branch 'main' of https://github.com/Hash-Nomads/edge-index-frontend into main

--- components/borrow/pools.tsx
+++ components/borrow/pools.tsx
@@ -31,29 +31,18 @@ const columns = [
     key: "liquidity",
     dataIndex: "liquidity",
   },
-  {
-    title: "Buy",
-    key: "buy",
-    dataIndex: "buy",
-    render: (text: string) => (
-      <div className="bg-gradient-to-r from-green-400 to-blue-500 text-center px-6 py-2 rounded-lg shadow-lg font-bold text-white cursor-pointer">
-        {text}
-      </div>
-    ),
-  },
 ];
 
 const data = [
   {
-    id: 1,
+    key: 1,
     etf: "Terra Ecosystem ETF",
     assets: ["LUNA", "ANC", "MIR"],
     apy: "203%",
     liquidity: "987665",
-    buy: "BUY",
   },
   {
-    id: 2,
+    key: 2,
     etf: "All weather pool ETF",
     assets: [
       "mIAU",
@@ -71,31 +60,27 @@ const data = [
     ],
     apy: "753%",
     liquidity: "487665",
-    buy: "BUY",
   },
   {
-    id: 3,
+    key: 3,
     etf: "Savings Pool ETF",
     assets: ["aUST", "mIAU", "mSLV"],
     apy: "435%",
     liquidity: "768947",
-    buy: "BUY",
   },
   {
-    id: 4,
+    key: 4,
     etf: "Index&ETF Pool ETF",
     assets: ["mQQQ", "mSPY", "aUST"],
     apy: "267%",
     liquidity: "658893",
-    buy: "BUY",
   },
   {
-    id: 5,
+    key: 5,
     etf: "New Rise Pool ETF ",
     assets: ["mCOIN", "mTSLA", "mABNB"],
     apy: "328%",
     liquidity: "985473",
-    buy: "BUY",
   },
 ];
 
@@ -107,22 +92,30 @@ const ExpandedRow = () => {
 
 const Pools = () => {
 
-  const [expKeys, setExpKeys] = useState(data && data.map(i => i.id));
+  const [expKeys, setExpKeys] = useState(data && data.map(i => i.key));
   const openOrCloseAll = (type: boolean) => {
-    type ? setExpKeys([]) : setExpKeys(data && data.map(i => i.id));
+    type ? setExpKeys([]) : setExpKeys(data && data.map(i => i.key));
   };
 
   return (
     <Table
       columns={columns}
       dataSource={data}
+      onRow={r => {
+        return {
+          onClick: (event) => {
+            const newExp: any = expKeys.filter(i => i !== r.key);
+            setExpKeys(newExp);
+          }
+        }
+      }}
       pagination={false}
       onExpand={(b, r) => {
-        const newExp: any = b ? [...expKeys, r.id] : expKeys.filter(i => i !== r.id);
+        const newExp: any = b ? [...expKeys, r.key] : expKeys.filter(i => i !== r.key);
         setExpKeys(newExp);
       }}
       expandedRowRender={record => ExpandedRow()}
-      rowKey={record => record.id}
+      rowKey={record => record.key}
       scroll={{ x: true }}
     />
   );
