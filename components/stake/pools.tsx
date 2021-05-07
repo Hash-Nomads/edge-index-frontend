4629d2ba352323cae14d4227c6b8971ddd448776
Waranyu-Mark26 <32408190+Waranyu-Mark26@users.noreply.github.com>
2021-05-08 02:32:36 +0700

    optimize token list

--- components/stake/pools.tsx
+++ components/stake/pools.tsx
@@ -3,11 +3,11 @@ import PoolCard from './pool-card'
 const Pools = () => {
   return (
     <div className="flex flex-wrap justify-center mb-24">
-      <PoolCard stakedTokenSymbolDisplay="TECO" stakedTokenSymbols={['LUNA', 'MIR', 'ANC']} earnedToken="TECO" />
-      <PoolCard stakedTokenSymbolDisplay="AWP" stakedTokenSymbols={['wMIR', 'SLV', 'LUNA', 'ETC']} earnedToken="AWP"  />
-      <PoolCard stakedTokenSymbolDisplay="SAVE" stakedTokenSymbols={['UST', 'wMIR', 'SLV']} earnedToken="SAVE"  />
-      <PoolCard stakedTokenSymbolDisplay="IND" stakedTokenSymbols={['QQQ', 'SPY', 'UST']} earnedToken="IND"  />
-      <PoolCard stakedTokenSymbolDisplay="RISE" stakedTokenSymbols={['mCOIN', 'TSLA', 'ABNB']} earnedToken="RISE" />
+      <PoolCard stakedTokenSymbolDisplay="TECO" earnedToken="TECO" />
+      <PoolCard stakedTokenSymbolDisplay="AWP" earnedToken="AWP"  />
+      <PoolCard stakedTokenSymbolDisplay="SAVE" earnedToken="SAVE"  />
+      <PoolCard stakedTokenSymbolDisplay="IND" earnedToken="IND"  />
+      <PoolCard stakedTokenSymbolDisplay="RISE" earnedToken="RISE" />
     </div>
   )
 }
