a94174dfea04f7ddd61d859b66c2ba174c421ea4
Waranyu-Mark26 <32408190+Waranyu-Mark26@users.noreply.github.com>
2021-05-08 01:01:10 +0700

    change some style

---
+++ components/borrow/header.tsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="text-3xl md:text-6xl font-bold text-center tracking-tight md:tracking-tighter leading-tight mt-8">
+        Borrow
+      </h1>
+      <h2 className="text-xl md:text-3xl text-center tracking-tight md:tracking-tighter leading-tight mb-20">
+        Leverage your position for more reward!
+      </h2>
+    </>
+  )
+}
+
+export default Header
