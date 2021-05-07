c5ac4ab5cf8702c2f55b7ddd9f4937b5c13d75cf
Waranyu-Mark26 <32408190+Waranyu-Mark26@users.noreply.github.com>
2021-05-07 21:44:31 +0700

    added token

--- components/stake/pool-card.tsx
+++ components/stake/pool-card.tsx
@@ -1,77 +1,52 @@
-const PoolCard = () => {
+import { FunctionComponent } from 'react';
+import Image from 'next/image'
+
+type IProps = {
+  stakedTokenSymbolDisplay: string
+  stakedTokenSymbols: Array<string>
+  earnedToken: string
+}
+
+const PoolCard: FunctionComponent<IProps> = ({stakedTokenSymbolDisplay, stakedTokenSymbols, earnedToken}: IProps) => {
+
   return (
     <>
-      <div className="p-4 lg:w-1/3">
-        <div className="bg-gray-200 bg-opacity-75 px-8 py-5 rounded-t-2xl overflow-hidden relative flex-col">
-          <div className="flex flex-wrap md:flex-nowrap">
+      <div className="p-4 lg:w-1/3 w-full">
+        <div className="bg-gradient-to-l from-blue-900 to-purple-800 bg-opacity-75 px-8 py-5 rounded-t-2xl overflow-hidden relative flex-col">
+          <div className="flex flex-wrap">
             <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <span className="text-2xl font-semibold title-font text-gray-700">
-                CATEGORY
+              <span className="text-2xl font-semibold title-font text-white">
+                Earn {earnedToken}
               </span>
-              <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+              <span className="mt-1 text-white text-sm">Stake {stakedTokenSymbolDisplay}</span>
             </div>
-            <div className="flex-grow flex justify-end items-center">
-              LOGO
+            <div className="flex-grow flex justify-end items-center text-white relative">
+              {stakedTokenSymbols.map((symbol, i) =>
+                <div key={symbol} style={{right: `${i*2}rem`}} className="absolute">
+                  <Image src={`/assets/tokens/${symbol}.png`} alt="" width="52" height="52" />
+                </div>
+              )}
             </div>
           </div>
         </div>
-        <div className="bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-b-2xl overflow-hidden text-center relative">
-          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-            CATEGORY
+        <div className="bg-gray-100 bg-opacity-75 px-8 pt-12 pb-12 rounded-b-2xl overflow-hidden text-center relative">
+          <h2 className="tracking-widest text-md title-font font-medium text-gray-400 mb-1">
+            APR
           </h2>
-          <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-            Raclette Blueberry Nextious Level
+          <h1 className="title-font sm:text-3xl text-2xl font-medium text-gray-900 mb-3">
+            20.72%
           </h1>
-          <p className="leading-relaxed mb-3">
-            Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-            microdosing tousled waistcoat.
-          </p>
-          <a className="text-indigo-500 inline-flex items-center">
-            Learn More
-            <svg
-              className="w-4 h-4 ml-2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M5 12h14"></path>
-              <path d="M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-          <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-            <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-              <svg
-                className="w-4 h-4 mr-1"
-                stroke="currentColor"
-                strokeWidth="2"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                viewBox="0 0 24 24"
-              >
-                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                <circle cx="12" cy="12" r="3"></circle>
-              </svg>
-              1.2K
-            </span>
-            <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-              <svg
-                className="w-4 h-4 mr-1"
-                stroke="currentColor"
-                strokeWidth="2"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                viewBox="0 0 24 24"
-              >
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>
-              6
+          <div className="flex justify-center items-center flex-wrap">
+            <Image src={`/assets/tokens/${earnedToken}.png`} alt="" width="52" height="52" />
+            <span className="tracking-widest text-xl title-font font-medium text-gray-400 mb-1 ml-3">
+              {earnedToken} Earned
             </span>
           </div>
+          <h1 className="title-font sm:text-3xl text-2xl font-medium text-gray-900 mb-3">
+            11.12
+          </h1>
+          <button className="text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded-lg text-lg h-10">Harvest</button>
+          <button className="text-white bg-blue-500 mt-5 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded-lg text-lg w-full h-16">Approve</button>
         </div>
       </div>
     </>
--- components/stake/pools.tsx
+++ components/stake/pools.tsx
@@ -3,11 +3,11 @@ import PoolCard from './pool-card'
 const Pools = () => {
   return (
     <div className="flex flex-wrap justify-center -m-4">
-      <PoolCard />
-      <PoolCard />
-      <PoolCard />
-      <PoolCard />
-      <PoolCard />
+      <PoolCard stakedTokenSymbolDisplay="ETF" stakedTokenSymbols={['LUNA', 'MIR', 'ANC']} earnedToken="ETF" />
+      <PoolCard stakedTokenSymbolDisplay="ETF" stakedTokenSymbols={['LUNA', 'MIR', 'ANC', 'BNB']} earnedToken="ETF"  />
+      <PoolCard stakedTokenSymbolDisplay="ETF" stakedTokenSymbols={['LUNA', 'ANC']} earnedToken="ETF"  />
+      <PoolCard stakedTokenSymbolDisplay="BNB" stakedTokenSymbols={['BNB']} earnedToken="ETF"  />
+      <PoolCard stakedTokenSymbolDisplay="ETH" stakedTokenSymbols={['ETH']} earnedToken="ETF" />
     </div>
   )
 }
